76bcefe43c88c31b96355edb1e33ececf7a26880
IllBo <83539496+IllBo@users.noreply.github.com>
2021-12-26 16:16:25 +0300

    Неожиданный поворот

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A multiuser text adventure game/learning project."
   :main ^:skip-aot mire.server
   :dependencies [[org.clojure/clojure "1.10.3"]
+				 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [server-socket "1.0.0"]])
