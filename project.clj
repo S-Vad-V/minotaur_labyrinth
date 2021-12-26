7e5b84964fecc660e257ba9d2e202e939733f632
mbolshakov2000 <michaelbolshakov@me.com>
2021-12-26 20:50:57 +0300

    small fix

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A multiuser text adventure game/learning project."
   :main ^:skip-aot mire.server
   :dependencies [[org.clojure/clojure "1.10.3"]
+				 [org.clojure/core.async "0.4.500"]
                  [server-socket "1.0.0"]])
